1a008ff3c6680f62ec444d057b0cc3661c80a104
Ammar Mohamed Sobhi <ammarmohamed13@gmail.com>
2021-09-19 22:34:31 +0200

    Update tutorial_second_page.dart

--- lib/components/tutorial_second_page.dart
+++ lib/components/tutorial_second_page.dart
@@ -4,7 +4,7 @@ class TutorialSecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-      'يمكنك تفعيل إشعارات للتذكير بأذكار الصباح من قائمة الإعدادات وتغيير حجم خط الأذكار وألوان التطبيق.',
+      'يمكنك تفعيل إشعارات للتذكير بأذكار الصباح والمساء من قائمة الإعدادات وتغيير حجم خط الأذكار وألوان التطبيق.',
       textAlign: TextAlign.justify,
       style: TextStyle(
         fontSize: 25.0,
